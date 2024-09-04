166efa7a4f545a11f73afc2da5df20790a50d643
Sai Karaskar <saikaraskar9@gmail.com>
2024-09-04 15:04:47 +0530

    renamed:    hero.html -> index.html

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
-const arr = ["rock","paper","scissors"]
+let arr = ["rock", "paper", "scissors"]
 
-function getcomputerchoice(){
-    const value = Math.floor(Math.random()* arr.length)
-    console.log(arr[value])
+function computerchoice() {
+    let choice = arr[Math.floor(Math.random() * arr.length)];
+    console.log("compchoice : " + choice)
+    return choice;
 }
 
-getcomputerchoice()
+function humanchoice() {
+    let humanchoice = prompt("enter rock paper or scissors :")
+    console.log("userchoice : " + humanchoice)
+    return humanchoice.toLowerCase();
+}
+
+let humanscore = 0;
+let computerscore = 0;
+
+function playround(humanchoice, computerchoice) {
+    if(humanchoice === computerchoice){
+        console.log("tie")
+    }
+    else if(
+        (humanchoice === "rock" && computerchoice === "scissors") ||
+        (humanchoice === "paper" && computerchoice === "rock")
+        (humanchoice === "scissors" && computerchoice === "paper")
+    ){
+        console.log("You win!")
+    }
+    else{
+        console.log("You Lose!")
+    }
+}
+
+playround(humanchoice(), computerchoice())
